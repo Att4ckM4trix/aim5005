6466fed127fbeda51c11397d165300e21bf73a5d
yuval nitzan <yuvaln4@gmail.com>
2025-03-03 19:51:11 -0500

    submission

--- aim5005/features.py
+++ aim5005/features.py
@@ -31,7 +31,7 @@ class MinMaxScaler:
         diff_max_min = self.maximum - self.minimum
         
         # TODO: There is a bug here... Look carefully! 
-        return x-self.minimum/(self.maximum-self.minimum)
+        return (x-self.minimum)/(self.maximum-self.minimum)
     
     def fit_transform(self, x:list) -> np.ndarray:
         x = self._check_is_array(x)
@@ -42,4 +42,18 @@ class MinMaxScaler:
 class StandardScaler:
     def __init__(self):
         self.mean = None
-        raise NotImplementedError
+        self.std = None
+    
+    def fit(self, x:np.ndarray) -> None:
+        self.mean = np.mean(x)
+        self.std = np.std(x)
+        
+    def transform(self, x:np.ndarray) -> np.ndarray:
+        # (x - u) / s
+        return (x - self.mean) / self.std
+    
+    def fit_transform(self, x:list) -> np.ndarray:
+        self.fit(x)
+        return self.transform(x)
+    
+    
